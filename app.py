d07220493e5210f9e0812c99fbe9c381300ce3b8
CaedusSolo <86660931+CaedusSolo@users.noreply.github.com>
2024-03-27 22:37:14 +0800

    update app.py, index.html

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        "id" : 1,
+        "title" : "Software Engineer",
+        "location" : "Kuala Lumpur",
+        "salary" : "MYR 10,000"
+    },
+    {
+        "id" : 2,
+        "title" : "Penetration Tester",
+        "location" : "Los Angeles, California, USA",
+        "salary" : "MYR 8,000"
+    },
+    {
+        "id" : 3,
+        "title" : "Software Tester",
+        "location" : "Cyberjaya, Selangor",
+        "salary" : "MYR 12,000"
+    },
+]
+
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('index.html',
+                           jobs=JOBS,
+                           company_name="Jovial Careers")
 
 
 if __name__ == "__main__":
