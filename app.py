8e4ddf18cd8c85fa4df03f0ac3c5614e49c091db
CaedusSolo <86660931+CaedusSolo@users.noreply.github.com>
2024-03-29 20:06:47 +0800

    fetching data from database

--- app.py
+++ app.py
@@ -1,38 +1,23 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        "id" : 1,
-        "title" : "Software Engineer",
-        "location" : "Kuala Lumpur",
-        "salary" : "MYR 10,000"
-    },
-    {
-        "id" : 2,
-        "title" : "Penetration Tester",
-        "location" : "Los Angeles, California, USA",
-        "salary" : "MYR 8,000"
-    },
-    {
-        "id" : 3,
-        "title" : "Software Tester",
-        "location" : "Cyberjaya, Selangor",
-        "salary" : "MYR 12,000"
-    },
-]
+
 
 @app.route("/")
 def hello():
+    jobs = load_jobs_from_db()
     return render_template('index.html',
-                           jobs=JOBS,
+                           jobs=jobs,
                            company_name="Jovial")
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS) 
+    jobs = load_jobs_from_db()
+
+    return jsonify(jobs) 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
--- database.py
+++ database.py
@@ -1,8 +1,15 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text   #  import necessary libraries and modules
+import creds
+#  link to connect to database
+engine = create_engine(creds.db_connection_string)
 
-db_connection_string = "mysql+pymysql://cyttestproject_continued:a0491153c4c73a17f4972591d7accf9fb75c47a7@x3o.h.filess.io:3307/cyttestproject_continued?charset=utf8mb4"
-engine = create_engine(db_connection_string)
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
 
-with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    print(result.all())
+        result_all = result.all()
+        jobs = []
+        for row in result_all:
+            jobs.append(dict(row._mapping))
+    
+        return jobs
