31b1ca89af9b3bf2d9f1ba8f81b094d4b91cbe75
CaedusSolo <86660931+CaedusSolo@users.noreply.github.com>
2024-03-29 23:32:34 +0800

    stored applications in database

--- database.py
+++ database.py
@@ -20,7 +20,8 @@ def load_jobs_from_db():
             jobs.append(dict(row._mapping))
     
         return jobs
-    
+
+
 def load_job_from_db(id):
     with engine.connect() as conn:
         result = conn.execute(
@@ -32,3 +33,18 @@ def load_job_from_db(id):
             return row._asdict()
         else:
             return None
+
+
+def add_application_to_db(job_id, application):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        
+        conn.execute(query, {'job_id' : job_id,
+                             'full_name' : application['full_name'],
+                             'email' : application['email'],
+                             'linkedin_url' : application['linkedin_url'],
+                             'education' : application['education'],
+                             'work_experience' : application['work_experience'],
+                             "resume_url" : application['resume_url']
+                             })
+        conn.commit()
